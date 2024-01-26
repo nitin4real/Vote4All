0cef0ab5afbbb8dd81a6f166819d3c1d7af66de6
nitinSingh <nitin.singh@edfora.com>
2024-01-26 13:20:35 +0530

    initial server setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mongoose = require('mongoose');
+
+const app = express()
+app.use(express.json())
+app.listen(4000,()=>console.log("started server on 4000"))
+app.get('/login',(req,res)=>{
+    console.log('req,res',req,res)
+    res.send('<h1>helloworld!</h1>')
+});
+
+app.post('/login', (req, res) => {
+    // const { id, password } = req.body
+    console.log('attemptlogin with ', req.body, req.query)  
+    res.send('working')
+})
