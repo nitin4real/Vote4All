d4bfcaca9e0211c16bbd6caa27f236400d9b1c05
nitinSingh <nitin.singh@edfora.com>
2024-01-27 22:21:01 +0530

    updated twilio verification flow

--- src/modules/signIn/helper/otp.helper.js
+++ src/modules/signIn/helper/otp.helper.js
@@ -3,28 +3,14 @@ const {
     TWILIO_API_KEY: authToken,
     TWILIO_SID: accountSid
 } = require('../../../Constants/config')
+const client = require('twilio')(accountSid, authToken);
 
-const twilioApiUrl = 'https://api.twilio.com/2010-04-01/Accounts/' + accountSid + '/Messages.json';
 const sendOtpHelper = async (phoneNumber, otp) => {
-    const postData = new URLSearchParams();
-    postData.append('To', '+' + phoneNumber);
-    postData.append('From', '+14452569089');
-    postData.append('Body', otp);
-    console.log('Twilio post data', postData)
-    axios.post(twilioApiUrl, postData, {
-        auth: {
-            username: accountSid,
-            password: authToken
-        },
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    })
-        .then(response => {
-            console.log('Twilio API response:', response.data);
-        })
-        .catch(error => {
-            console.error('Error making Twilio API request:', error.response.data);
-        });
+    try{
+        await client.messages.create({ from: '+14452569089', body: 'Your Vote4All Otp is - ' + otp, to: '+' + phoneNumber })
+        console.log( 'messege sent to ', phoneNumber, otp)
+    } catch (err){
+        console.log('Error In Sending Otp')
+    }
 }
 module.exports = sendOtpHelper
